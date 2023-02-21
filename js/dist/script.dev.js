ff915f52a716c872ad45bcdb1c2ebde4a1c1fb7c
oleksenko02 <123555936+oleksenko02@users.noreply.github.com>
2023-02-21 20:34:02 +0800

    Add files via upload

---
+++ js/dist/script.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+if (2 * 4 == 8 * 1) {
+  console.log('Yes');
+} else {
+  console.log('no');
+}
---
+++ js/script.js
@@ -0,0 +1,5 @@
+if (2 * 4 == 8 * 1) {
+    console.log('Yes')
+} else {
+    console.log('no')
+}
